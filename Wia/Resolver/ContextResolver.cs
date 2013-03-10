79fb1d48813a4dbd6d03fa3a23694dce097c527c
Andreas <andreas@thecodingbug.com>
2013-03-10 21:38:46 +0100

    Reorder methods in ContextResolver

--- Wia/Resolver/ContextResolver.cs
+++ Wia/Resolver/ContextResolver.cs
@@ -21,6 +21,106 @@ namespace Wia.Resolver {
             context.EpiserverVersion = GetEpiserverVersion(context);
         }
 
+        private static string GetProjectName(WebsiteContext context) {
+            if (context.ExitAtNextCheck)
+                return null;
+
+            string solutionFileName = Directory.EnumerateFiles(context.CurrentDirectory).FirstOrDefault(x => x.EndsWith(".sln"));
+            string projectName = context.ProjectName;
+
+            if (solutionFileName.IsNullOrEmpty()) {
+                context.ExitAtNextCheck = true;
+                Console.WriteLine("Could not find a solution file in the current directory.");
+            }
+
+            if (string.IsNullOrWhiteSpace(projectName)) {
+                projectName = Path.GetFileNameWithoutExtension(solutionFileName);
+            }
+
+            return projectName;
+        }
+
+        private static string GetWebProjectName(WebsiteContext context) {
+            if (context.ExitAtNextCheck)
+                return null;
+
+            string webProjectName = context.WebProjectName;
+            if (string.IsNullOrWhiteSpace(webProjectName)) {
+                var webDirectories = Directory.EnumerateDirectories(context.CurrentDirectory)
+                                              .Where(dir => Path.GetFileName(dir).Contains("Web"))
+                                              .ToList();
+
+                if (!webDirectories.Any()) {
+                    context.ExitAtNextCheck = true;
+                    Console.WriteLine("Web project could not be resolved. Please specifiy using \"--webproject Project.Web\". \n");
+                    return null;
+                }
+
+                if (webDirectories.Count > 1) {
+                    context.ExitAtNextCheck = true;
+                    Console.WriteLine("You need to specificy which web project to use: \n--webproject " +
+                        webDirectories.Select(path => Path.GetFileName(path)).Aggregate((a, b) => a + "\n--webproject " + b) + "\n");
+                    return null;
+                }
+
+                webProjectName = Path.GetFileName(webDirectories.FirstOrDefault());
+            }
+
+            var projectDirectory = Path.Combine(context.CurrentDirectory, webProjectName);
+
+            if (!Directory.Exists(projectDirectory)) {
+                context.ExitAtNextCheck = true;
+                Console.WriteLine("Web project directory does not seem to exist at " + projectDirectory);
+                return null;
+            }
+
+            return webProjectName;
+        }
+
+        private static string GetProjectUrl(WebsiteContext context) {
+            if (context.ExitAtNextCheck)
+                return null;
+
+            var projectUrl = context.ProjectUrl;
+
+            if (!string.IsNullOrWhiteSpace(projectUrl)) {
+                if (!projectUrl.StartsWith("http://"))
+                    projectUrl = "http://" + projectUrl;
+
+                return projectUrl;
+            }
+
+            var webProjectFolderPath = context.GetWebProjectDirectory();
+            var episerverConfigPath = Directory.GetFiles(webProjectFolderPath, "episerver.config", SearchOption.AllDirectories)
+                                               .FirstOrDefault();
+
+            if (!File.Exists(episerverConfigPath)) {
+                // the episerver config section might be in web.config
+                var webConfigPath = Path.Combine(webProjectFolderPath, "web.config");
+
+                if (!File.Exists(webConfigPath)) {
+                    // serious trouble if this file can not be found.
+                    context.ExitAtNextCheck = true;
+                    Console.WriteLine("The web.config file could not be found in " + webProjectFolderPath);
+                    return null;
+                }
+
+                episerverConfigPath = webConfigPath;
+            }
+
+            var doc = XDocument.Load(episerverConfigPath);
+            var siteSettings = doc.Descendants().FirstOrDefault(x => x.Name.LocalName == "siteSettings");
+
+            if (siteSettings == null) {
+                context.ExitAtNextCheck = true;
+                Console.WriteLine("Could not find the EPiServer configuration section in neither episerver.config or web.config.");
+                return null;
+            }
+
+            projectUrl = siteSettings.Attribute("siteUrl").Value;
+            return projectUrl;
+        }
+
         private static double GetFrameworkVersion(WebsiteContext context) {
             if (context.ExitAtNextCheck)
                 return -1;
@@ -75,106 +175,6 @@ namespace Wia.Resolver {
             }
 
             return -1;
-        }
-
-        private static string GetWebProjectName(WebsiteContext context) {
-            if (context.ExitAtNextCheck)
-                return null;
-
-            string webProjectName = context.WebProjectName;
-            if (string.IsNullOrWhiteSpace(webProjectName)) {
-                var webDirectories = Directory.EnumerateDirectories(context.CurrentDirectory)
-                                              .Where(dir => Path.GetFileName(dir).Contains("Web"))
-                                              .ToList();
-
-                if (!webDirectories.Any()) {
-                    context.ExitAtNextCheck = true;
-                    Console.WriteLine("Web project could not be resolved. Please specifiy using \"--webproject Project.Web\". \n");
-                    return null;
-                }
-
-                if (webDirectories.Count > 1) {
-                    context.ExitAtNextCheck = true;
-                    Console.WriteLine("You need to specificy which web project to use: \n--webproject " + 
-                        webDirectories.Select(path => Path.GetFileName(path)).Aggregate((a, b) => a + "\n--webproject " + b) + "\n");
-                    return null;
-                }
-
-                webProjectName = Path.GetFileName(webDirectories.FirstOrDefault());
-            }
-
-            var projectDirectory = Path.Combine(context.CurrentDirectory, webProjectName);
-
-            if (!Directory.Exists(projectDirectory)) {
-                context.ExitAtNextCheck = true;
-                Console.WriteLine("Web project directory does not seem to exist at " + projectDirectory);
-                return null;
-            }
-
-            return webProjectName;
-        }
-
-        private static string GetProjectName(WebsiteContext context) {
-            if (context.ExitAtNextCheck)
-                return null;
-
-            string solutionFileName = Directory.EnumerateFiles(context.CurrentDirectory).FirstOrDefault(x => x.EndsWith(".sln"));
-            string projectName = context.ProjectName;
-
-            if (solutionFileName.IsNullOrEmpty()) {
-                context.ExitAtNextCheck = true;
-                Console.WriteLine("Could not find a solution file in the current directory.");
-            }
-
-            if (string.IsNullOrWhiteSpace(projectName)) {
-                projectName = Path.GetFileNameWithoutExtension(solutionFileName);
-            }
-
-            return projectName;
-        }
-
-        private static string GetProjectUrl(WebsiteContext context) {
-            if (context.ExitAtNextCheck)
-                return null;
-
-            var projectUrl = context.ProjectUrl;
-
-            if (!string.IsNullOrWhiteSpace(projectUrl)) {
-                if (!projectUrl.StartsWith("http://"))
-                    projectUrl = "http://" + projectUrl;
-
-                return projectUrl;
-            }
-
-            var webProjectFolderPath = context.GetWebProjectDirectory();
-            var episerverConfigPath = Directory.GetFiles(webProjectFolderPath, "episerver.config", SearchOption.AllDirectories)
-                                               .FirstOrDefault();
-
-            if (!File.Exists(episerverConfigPath)) {
-                // the episerver config section might be in web.config
-                var webConfigPath = Path.Combine(webProjectFolderPath, "web.config");
-
-                if (!File.Exists(webConfigPath)) {
-                    // serious trouble if this file can not be found.
-                    context.ExitAtNextCheck = true;
-                    Console.WriteLine("The web.config file could not be found in " + webProjectFolderPath);
-                    return null;
-                }
-
-                episerverConfigPath = webConfigPath;
-            }
-
-            var doc = XDocument.Load(episerverConfigPath);
-            var siteSettings = doc.Descendants().FirstOrDefault(x => x.Name.LocalName == "siteSettings");
-
-            if (siteSettings == null) {
-                context.ExitAtNextCheck = true;
-                Console.WriteLine("Could not find the EPiServer configuration section in neither episerver.config or web.config.");
-                return null;
-            }
-
-            projectUrl = siteSettings.Attribute("siteUrl").Value;
-            return projectUrl;
         }
     }
 }
